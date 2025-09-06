06c55ba56809fc6fb7ff5455ecb4659f1318c013
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-09-06 22:48:11 +0300

    Create DB and configurations

---
+++ CleanArchitecture.Persistence/Configurations/CarConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Persistence.Configurations;
+
+public sealed class CarConfiguration : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.ToTable("Cars");
+        builder.HasKey(c => c.Id);
+        builder.HasIndex(c => c.Id).IsUnique();
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(c => c.Model)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(c => c.EnginePower)
+            .IsRequired();
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,15 +1,20 @@
+using CleanArchitecture.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("SqlServerConnection")!;
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(connectionString));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
