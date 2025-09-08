2824b33a34a0c1de42af2fc0bd4fb23869d874f2
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-09-09 00:06:43 +0300

    add repository pattern and update .net 9

--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,17 +1,23 @@
 using CleanArchitecture.Application.Behaviors;
 using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Repositories;
 using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
 using CleanArchitecture.Persistence.Services;
 using CleanArchitecture.WebApi.Middlewares;
 using FluentValidation;
+using GenericRepository;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ExceptionMiddleware>();
 
 builder.Services.AddScoped<ICarService, CarService>();
+builder.Services.AddScoped<IUnitOfWork>(srv => srv.GetRequiredService<AppDbContext>());
+builder.Services.AddScoped<ICarRepository, CarRepository>();
 
 builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistence.AssemblyReference).Assembly);
 
@@ -28,15 +34,15 @@ builder.Services.AddMediatR(cfr =>
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 builder.Services.AddValidatorsFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly);
 
+builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference(sc => sc.Layout = ScalarLayout.Classic);
 }
 
 app.UseMiddlewareExtensions();
